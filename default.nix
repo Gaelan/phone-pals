4f974039aa460e0726edf852506cdd634ec7fef7
Gaelan Steele <gbs@canishe.com>
2020-07-01 12:25:43 -0700

    nix updates

--- default.nix
+++ default.nix
@@ -1,21 +1,31 @@
-with (import <nixpkgs> {});
+{ pkgs ? import <nixpkgs> { }, env ? "development", masterKey ? null }:
 
 let
-  rubyenv = bundlerEnv {
-    name = "rb";
+  rubyenv = pkgs.bundlerEnv {
+    name = "phone-pals-rubyenv";
     # Setup for ruby gems using bundix generated gemset.nix
-    ruby = ruby_2_7;
+    ruby = pkgs.ruby_2_7;
     gemfile = ./Gemfile;
     lockfile = ./Gemfile.lock;
     gemset = ./gemset.nix;
     # Bundler groups available in this environment
-    groups = ["default" "development" "test"];
+    groups = [ "default" "development" "test" ];
   };
-in stdenv.mkDerivation {
+  nodeModules = pkgs.yarn2nix-moretea.mkYarnModules {
+    name = "phone-pals-nodeModules";
+    pname = "phone_pals";
+    version = "0.1.0";
+    packageJSON = ./package.json;
+    yarnLock = ./yarn.lock;
+    yarnNix = ./yarn.nix;
+  };
+in pkgs.stdenv.mkDerivation {
   name = "phone-pals";
   version = "0.0.1";
 
-  buildInputs = [
+  src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
+
+  buildInputs = with pkgs; [
     stdenv
     git
 
@@ -23,6 +33,9 @@ in stdenv.mkDerivation {
     ruby_2_7
     bundler
     bundix
+
+    # Node deps
+    yarn2nix
 
     # Rails deps
     clang
@@ -37,8 +50,27 @@ in stdenv.mkDerivation {
     rubyenv
   ];
 
+  RAILS_ENV = env;
+  RAILS_MASTER_KEY = masterKey;
+
   shellHook = ''
     export LIBXML2_DIR=${pkgs.libxml2}
     export LIBXSLT_DIR=${pkgs.libxslt}
+  '';
+
+  buildPhase = ''
+    cp -r ${nodeModules}/node_modules node_modules
+
+    # HACK: disable yarn
+    mv bin/yarn bin/xyarn
+    touch bin/yarn
+    chmod +x bin/yarn
+    rails assets:precompile
+    rm bin/yarn
+    mv bin/xyarn bin/yarn
+  '';
+
+  installPhase = ''
+    cp -r . $out
   '';
 }
